a8fd24b6a570657813ae3d3f6b6c57ee7a150f74
APB98 <borja.basolutions@gmail.com>
2021-07-27 13:44:29 -0500

    changes, addition of the service-worker.js file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,11 @@
 // Here goes all the cache files
+// Create a constant that will store all the files that will be stored on the cache
 
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/offline.js",
+  "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
+  "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
+];
